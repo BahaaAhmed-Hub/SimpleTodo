b2c92f9be6f9439d56e1c31b7ea50600b589bbbc
Bahaa Ahmed <bahaaahmed@Bahaas-MacBook-Pro.local>
2024-12-28 12:02:44 +0200

    Initial commit

---
+++ backend/src/main/java/com/simpletodo/controller/TaskController.java
@@ -0,0 +1,38 @@
+package com.simpletodo.controller;
+
+import com.simpletodo.model.Task;
+import com.simpletodo.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task completeTask(@PathVariable Long id) {
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setCompleted(true);
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+}
